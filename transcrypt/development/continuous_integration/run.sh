2ed645145f3b4dde280cfe359eb05455591acd09
JdeH <info@qquick.org>
2018-10-07 20:04:16 +0200

    ci test y18m10d07 h20m04s00

--- transcrypt/development/continuous_integration/run.sh
+++ transcrypt/development/continuous_integration/run.sh
@@ -2,7 +2,12 @@
 chmod -R 777 .
 
 # Start headless browser instance, Python's webbrowser.open will now refer to this instance
-- google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
+# - google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
+
+xdg-mime default google-chrome.desktop text/html
+xdg-mime default google-chrome.desktop x-scheme-handler/http
+xdg-mime default google-chrome.desktop x-scheme-handler/https
+xdg-mime default google-chrome.desktop x-scheme-handler/about
 
 # Enable shipment_test.py to find run_python
 export PATH=$PATH:./transcrypt
