f0476239fa9990c2c20390f7c8c437f8bda31be9
JdeH <info@qquick.org>
2018-10-07 18:48:26 +0200

    ci test y18m10d07 h18m48s00

--- transcrypt/development/continuous_integration/run.sh
+++ transcrypt/development/continuous_integration/run.sh
@@ -2,7 +2,8 @@
 chmod -R 777 .
 
 # Start headless browser instance, Python's webbrowser.open will now refer to this instance
-google-chrome-stable --headless --disable-gpu
+# google-chrome-stable --headless --disable-gpu
+chrome --headless --disable-gpu
 
 # Enable shipment_test.py to find run_python
 export PATH=$PATH:./transcrypt
